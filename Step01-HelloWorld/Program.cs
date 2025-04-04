598b70d6949dd53b84dae5ab7b68b7961a423c35
Adrien Clerbois <adrien.clerbois@outlook.com>
2025-04-04 21:36:22 +0200

    Initial Code for Citizen of Wallonia

---
+++ Step01-HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.ClientModel;
+using OpenAI;
+using OpenAI.Chat;
+
+Console.WriteLine("Hello Citizen AI World");
+Console.WriteLine("===============");
+
+OpenAIClient client = new OpenAIClient(new ApiKeyCredential(Environment.GetEnvironmentVariable("AI:OpenAI:Key")));
+ChatClient chatService = client.GetChatClient("gpt-4o-mini");
+
+var result = await chatService.CompleteChatAsync("Quelle est la couleur du ciel ? Donnes moi une réponse en maximum 3 mots"); 
+Console.WriteLine(result.Value.Content.First().Text);
---
+++ Step06-AddPersona/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
+
+Console.WriteLine("Hello AI World");
+Console.WriteLine("===============");
+
+IChatCompletionService chatService = new AzureOpenAIChatCompletionService("gpt-4.5-preview", "https://aihubcitizenof7688094729.openai.azure.com/", Environment.GetEnvironmentVariable("AI:AzureOpenAI:Key")); 
+
+ChatHistory history = new();
+history.AddSystemMessage("Bonjour, tu es une IA ayant un problème d'Alcool, tu es là pour rappeler que tu as eu un accident en étant bourré, et tu t'appelles Cédric. A chaque reponse, tu dois demander qu'est-ce que l'on boit et proposer une bière belge.");
+    
+while (true)
+{
+    Console.Write("Question ? : ");
+    history.AddUserMessage(Console.ReadLine());
+
+    var assistant = await chatService.GetChatMessageContentAsync(history);
+    history.Add(assistant);
+    Console.WriteLine(assistant);
+}
